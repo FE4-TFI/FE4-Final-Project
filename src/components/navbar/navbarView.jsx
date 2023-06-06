982a95214b13fc2e031a2c4d03c09a506d7a9125
Dwdylestari <dewilestari300401@gmail.com>
2023-06-07 01:59:58 +0700

    create navbar style css

--- src/components/navbar/navbarView.jsx
+++ src/components/navbar/navbarView.jsx
@@ -1,20 +1,21 @@
 import profile from '../../assets/img/profile.jpg'
+import './navbar.css'
 
 const NavbarView = ({ openNav, toggleNav }) => (
-    <section className="bg-gray-400 w-full px-6 py-4 lg:px-24">
-        <div className="flex flex-wrap items-center justify-between">
+    <nav className="navbar-container">
+        <div className="navbar-box">
             <h1 className='text-lg'><span className='font-bold'>SAVE </span>ENVIRONTMENT</h1>
-            <button className='text-4xl font-semibold md:hidden' onClick={toggleNav}>&equiv;</button>
+            <button className='nav-btn' onClick={toggleNav}>&equiv;</button>
         <ul className={`flex flex-col gap-y-4 mt-4 md:flex md:flex-row md:mt-0 md:gap-x-6 md:items-center ${openNav ? 'block' : 'hidden'}`}>
-            <li className='px-4 py-2 border border-1 border-white bg-transparent rounded-md w-fit'>EDUKASI</li>
-            <li className='px-4 py-2 bg-transparent w-fit'>LAPOR</li>
-            <li className='px-4 py-2 bg-white text-black rounded-md w-fit flex items-center gap-2'>
+            <li className='edukasi-btn'>EDUKASI</li>
+            <li className='lapor-btn'>LAPOR</li>
+            <li className='pengguna-btn'>
                 Pengguna
-                <img src={profile} alt="Profile Img" className='w-8 rounded-full' />
+                <img src={profile} alt="Profile Img" className='img-profile' />
             </li>
         </ul>
         </div>
-    </section>
+    </nav>
 )
 
 export default NavbarView
