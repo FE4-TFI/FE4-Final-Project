8f86ee0cda82497d0e0564cd4faeaf46f374d971
achmadtaufiqhidayat <ahmadtaufiqhidayat19@gmail.com>
2023-06-07 12:59:31 +0700

    menambah form lapor

--- src/pages/Lapor.jsx
+++ src/pages/Lapor.jsx
@@ -4,9 +4,40 @@ import Register from "./Register"
 function Lapor() {
     return (
         <>
-            <h1>ini page lapor</h1>
-            {/* <Link to="/login" className='btn btn-primary btn-md'>LOGIN</Link> */}
-            <Register />
+            <div>
+                <h2>DAFTAR</h2>
+
+                <form className="form-group custom-form">
+                    <div>
+                        <label>Nama Pelapor</label>
+                        <input className="form-control" type="text" placeholder="Masukkan Nama User" />
+                    </div>
+                    <div>
+                        <label htmlFor="">Email Pelapor</label>
+                        <input className="form-control" placeholder="Masukkan Email" type="email" />
+                    </div>
+                    <div>
+                        <label>No Hp Pelapor</label>
+                        <input className="form-control" type="number" />
+                    </div>
+                    <div>
+                        <label>Lokasi</label>
+                        <input className="form-control" type="text-area" />
+                    </div>
+                    <div>
+                        <label>Jenis Kejadian</label>
+                        <input className="form-control" type="password" />
+                    </div>
+                    <div>
+                        <label>Deskripsi Kejadian</label>
+                        <input className="form-control" type="password" />
+                    </div>
+                    <button className="btn btn-success" type="button" >
+                        {/* {loading ? "Mendaftarkan Akun..." : "Daftar"} */} KIRIM
+                    </button>
+                </form>
+
+            </div>
         </>
     )
 }
