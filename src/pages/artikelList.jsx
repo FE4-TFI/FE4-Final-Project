aaa7f108e237c632f22388f29b8173352e8730c8
Dwdylestari <111202013109@mhs.dinus.ac.id>
2023-06-14 03:31:45 +0700

    Membuat responsive css artikelList

--- src/pages/artikelList.jsx
+++ src/pages/artikelList.jsx
@@ -7,18 +7,17 @@ function ArtikelList () {
     // Mengambil data artikel by id dengan RTK Query
     const { data: artikel, isLoading: loadingArtikel, isError: errorArtikel, error: errorArt } = useGetArtikelByIdQuery(1);
 
-    if (loadingArtikel) return <h1>Loading....</h1>
     if (errorArtikel) return <h1>{errorArt.message}</h1>
     return (
         <div className="list-container">
             <div className="list-box">
                 <div>
-                    <h1 className="list-title">{artikel.title}</h1>
-                    <p className="list-text">{artikel.paragraf1}</p>
+                    <h1 className="list-title">{loadingArtikel ? <h1>Loading...</h1> : artikel.title}</h1>
+                    <p className="list-text">{loadingArtikel ? <h1>Loading...</h1> : artikel.paragraf1}</p>
                     <button className="btn-primary">Baca</button>
                 </div>
                 <div>
-                    <img src={hero} alt="" />
+                    <img src={hero} alt="...." className="list-img" />
                 </div>
             </div>
         </div>
